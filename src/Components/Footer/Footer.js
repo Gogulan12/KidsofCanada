04e8e0818f4b97bd67b574a204b876dd612685ea
Gogulan12 <gogulanravi12@gmail.com>
2023-07-10 21:50:54 -0400

    added footer and home information

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>Contact information</div>
+      <p>Kids of Canada ©️ 2023</p>
+    </div>
+  );
+}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navigation from "../Components/Navigation/Navigation";
 import "./Home.css";
+import Footer from "../Components/Footer/Footer";
 export default function Home() {
   return (
     <div className="home">
@@ -9,12 +10,7 @@ export default function Home() {
         <h2>Welcome to Kids of Canada</h2>
         <button>Register you child now</button>
       </div>
-      {/* <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit dolores
-        magnam rem illum optio dolorum, dolor perspiciatis doloribus velit in
-        voluptatibus blanditiis maxime, delectus fuga cumque hic, magni quia
-        sequi.
-      </p> */}
+
       <h2 className="programTitle">Programs</h2>
       <div className="programs">
         <a href="/Hockey">
@@ -36,10 +32,37 @@ export default function Home() {
 
       <div className="aboutContainer">
         <h2 className="programTitle">About Us</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+          explicabo dolorem facere ad quas consequuntur nesciunt similique
+          dolores eos temporibus ab repudiandae tempore nemo neque, veniam
+          debitis soluta. Facere veniam, mollitia debitis accusamus voluptatum
+          autem delectus suscipit molestias reiciendis? Ipsum veniam in
+          provident suscipit, nobis autem dolor. Sed, nobis quia?
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+          debitis deserunt sapiente obcaecati tempore dolor id minima asperiores
+          voluptas reprehenderit? Laboriosam voluptatibus eaque, at et id
+          blanditiis natus vitae! Fuga!
+        </p>
       </div>
       <div className="aboutContainer">
         <h2 className="programTitle">Contact Us</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, hic.
+          Facilis voluptas quibusdam a amet consequatur eaque veritatis
+          voluptatem ratione, magni blanditiis quasi numquam at. Iusto
+          perspiciatis a voluptate similique.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. At, eos!
+          Necessitatibus molestiae error delectus praesentium quasi quia
+          aspernatur corrupti quam sequi, aut porro, perferendis tempora unde
+          esse hic iusto aliquam.
+        </p>
       </div>
+      <Footer />
     </div>
   );
 }
